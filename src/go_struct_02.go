38636e3af862aef77cfc7fdad912a147c62e1086
abruption <first_kuls@naver.com>
2020-11-27 16:21:04 +0900

    11.27 구조체 v3

--- src/go_struct_02.go
+++ src/go_struct_02.go
@@ -8,19 +8,19 @@ type subscriber struct {
 	active bool
 }
 
-func printInfo(s subscriber) {
+func printInfo(s *subscriber) {
 	fmt.Println("Name : ", s.name)	
 	fmt.Println("Monthly Rate : ", s.rate)
 	fmt.Println("Active : ", s.active)
 	fmt.Println()
 }
 
-func defaultSubscriber(name string) subscriber {
+func defaultSubscriber(name string) *subscriber {
 	var s subscriber
 	s.name = name
 	s.rate = 5000
 	s.active = true
-	return s
+	return &s
 }	
 
 func applyDiscount(s *subscriber) {
@@ -29,7 +29,8 @@ func applyDiscount(s *subscriber) {
 
 func main() {
 	s1 := defaultSubscriber("kim")
-	s1.rate = 4500
+	//s1.rate = 4500
+	applyDiscount(s1)
 	printInfo(s1)
 
 	s2 := defaultSubscriber("park")
