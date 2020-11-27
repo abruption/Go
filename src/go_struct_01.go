7f8ed749494a7d6d64774a8fc620f85b09648a85
abruption <first_kuls@naver.com>
2020-11-27 15:48:22 +0900

    11.27 구조체 v3

--- src/go_struct_01.go
+++ src/go_struct_01.go
@@ -12,10 +12,21 @@ func showInfo(p part) {
 	fmt.Println("Count : ", p.count)
 }
 
+func minimumOrder(description string) part {
+        var p part
+        p.description = description
+        p.count = 100
+        return p
+}
+
 func main() {
+        p := minimumOrder("Hex bolts")
+        fmt.Println(p.description, p.count)
+
+	/*
 	var bolts part
 	bolts.description = "Hex bolts"
 	bolts.count = 100
 	showInfo(bolts)
-	
+	*/
 }
